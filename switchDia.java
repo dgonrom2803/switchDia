30d3a21aa347b9015835c4891653f0521ebed93d
dgonrom2803 <dgonrom2803@g.educaand.es>
2022-11-02 09:30:42 +0100

    Create switchDia.java

---
+++ switchDia.java
@@ -0,0 +1,26 @@
+public class switchDia () {
+    public static void main(String[] args) {
+        
+        int dia = 8;
+        switch (dia) {
+            case 1: 
+                System.out.println("Lunes");
+                break;
+            case 2:
+                System.out.println("Martes");
+                break;
+            case 3:
+                System.out.println("Miércoles");
+                break;
+            case 4:
+                System.out.println("Jueves");
+                break;
+            case 5:
+                System.out.println("Viernes");
+                break;
+            default:
+                System.out.println("Finde");
+                break;
+        }
+    }
+}
